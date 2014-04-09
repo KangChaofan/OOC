d2fc0d29ae9b9a6a8ec8887a55fdcc6f43367fd5
wings <wenqs27@gmail.com>
2014-04-09 14:02:43 +0800

    modify SerializableUtilTest  2014/04/09 周三 14:02:43.12

--- OOC.UnitTest/Util/SerializableUtilTest.cs
+++ OOC.UnitTest/Util/SerializableUtilTest.cs
@@ -36,8 +36,8 @@ namespace OOC.UnitTest.Util
             SerializableUtil.DeepCopy(_xiaoyun, out clone);
             _nobody = (Person)clone;
 
-            //TODO complete me.
-            Assert.IsNotNull(_nobody);
+            Assert.AreNotSame(_xiaoyun, _nobody);
+            Assert.AreEqual(_xiaoyun,_nobody);
         }
 
         [TestMethod]
@@ -54,9 +54,33 @@ namespace OOC.UnitTest.Util
         [Serializable]
         public class Person
         {
+            protected bool Equals(Person other)
+            {
+                return string.Equals(Name, other.Name) && Age == other.Age && Equals(Father, other.Father);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    int hashCode = (Name != null ? Name.GetHashCode() : 0);
+                    hashCode = (hashCode*397) ^ Age;
+                    hashCode = (hashCode*397) ^ (Father != null ? Father.GetHashCode() : 0);
+                    return hashCode;
+                }
+            }
+
             public string Name { get; set; }
             public int Age { get; set; }
             public Person Father { get; set; }
+            
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != GetType()) return false;
+                return Equals((Person) obj);
+            }
         }
     }
 }

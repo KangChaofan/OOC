d147b398fe45b68516c030c0a6aba1ffb1370c00
wings <wenqs27@gmail.com>
2014-04-12 19:26:55 +0800

    adjust directory and namespace, add main window.  2014/04/12 周六 19:26:55.16

--- OOC.GUI/FileClient/Properties/AssemblyInfo.cs
+++ OOC.GUI/FileClient/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("FileServiceClient")]
+[assembly: AssemblyTitle("FileClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FileServiceClient")]
+[assembly: AssemblyProduct("FileClient")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- OOC.GUI/FileClient/Properties/Settings.Designer.cs
+++ OOC.GUI/FileClient/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FileServiceClient.Properties
+namespace FileClient.Properties
 {
 
 

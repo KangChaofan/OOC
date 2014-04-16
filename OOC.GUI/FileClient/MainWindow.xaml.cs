e69ebe53b2cd1ef0352d7082648db46abcb02609
wings <wenqs27@gmail.com>
2014-04-16 23:07:14 +0800

    add some menu items.  2014/04/16 周三 23:07:14.06

--- OOC.GUI/FileClient/MainWindow.xaml.cs
+++ OOC.GUI/FileClient/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.ServiceModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using FileClient.Annotations;
 using FileClient.FileService;
 using FileClient.View;
 using FileClient.WindowEffect;
+using Microsoft.Win32;
 using OOC.Util;
 
 namespace FileClient
@@ -54,8 +56,8 @@ namespace FileClient
 
 
             rootPath = new FileItemView
-            {
-                SubItems = new ObservableCollection<FileItemView>
+                {
+                    SubItems = new ObservableCollection<FileItemView>
                         {
                             new FileItemView
                                 {
@@ -65,7 +67,7 @@ namespace FileClient
                                                                    UriKind.RelativeOrAbsolute)),
                                 }
                         }
-            };
+                };
             CurrentPath = rootPath;
 
             SetBindings();
@@ -74,7 +76,12 @@ namespace FileClient
         private void SetBindings()
         {
             TreeView.SetBinding(ItemsControl.ItemsSourceProperty,
-                                new Binding {Source = rootPath, Path = new PropertyPath("SubItems"),Mode = BindingMode.OneTime});
+                                new Binding
+                                    {
+                                        Source = rootPath,
+                                        Path = new PropertyPath("SubItems"),
+                                        Mode = BindingMode.OneTime
+                                    });
             ListView.SetBinding(ItemsControl.ItemsSourceProperty,
                                 new Binding
                                     {
@@ -97,6 +104,52 @@ namespace FileClient
             catch (FaultException e)
             {
                 //                _logger.Warn(e.Message);
+            }
+        }
+
+        private void NewConnection()
+        {
+        }
+
+        private void OpenConnection()
+        {
+        }
+
+        private void SaveConnection()
+        {
+        }
+
+        private void SaveConnectionAs()
+        {
+        }
+
+        private void DownFile()
+        {
+            var fileEntityResponse = Client.Get(CurrentPath.Name);
+            SaveFileDialog fileDialog = new SaveFileDialog
+            {
+                FileName = fileEntityResponse.FileName,
+                Filter =
+                    string.Format("Default (*{0})|*{0}|All Files (*.*)|*.*",
+                                  Path.GetExtension(fileEntityResponse.FileName)),
+            };
+            var showDialog = fileDialog.ShowDialog(this);
+            if (showDialog.Value)
+            {
+                IOUtil.WriteAllBytes(fileDialog.FileName, fileEntityResponse.Content);
+            }
+        }
+
+        private void UploadFile()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+            };
+            var showDialog = fileDialog.ShowDialog(this);
+            if (showDialog.Value)
+            {
+                var content = IOUtil.ReadAllBytes(fileDialog.FileName);
+                Client.Put(CurrentPath.Name, content);
             }
         }
 
@@ -163,11 +216,11 @@ namespace FileClient
             StatusText.Text = string.Format("已选中 {0}, 大小 {1}字节, 上次访问 {2}", _currentPath.DisplayName, _currentPath.Size,
                                             _currentPath.AccessTime);
             ListView.SetBinding(ItemsControl.ItemsSourceProperty,
-                    new Binding
-                    {
-                        Source = CurrentPath,
-                        Path = new PropertyPath("SubItems"),
-                    });
+                                new Binding
+                                    {
+                                        Source = CurrentPath,
+                                        Path = new PropertyPath("SubItems"),
+                                    });
 //            SetBindings();
         }
 
@@ -176,6 +229,35 @@ namespace FileClient
         {
             var handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            string header = e.Source.CastTo<MenuItem>().Header.ToString();
+            switch (header)
+            {
+                case "新建连接(_O)...":
+                    NewConnection();
+                    break;
+                case "打开连接(_O)...":
+                    OpenConnection();
+                    break;
+                case "保存连接(_S)":
+                    SaveConnection();
+                    break;
+                case "连接另存为(_A)...":
+                    SaveConnectionAs();
+                    break;
+                case "上传文件(_U)...":
+                    UploadFile();
+                    break;
+                case "下载文件(_D)...":
+                    DownFile();
+                    break;
+                case "退出(_Q)":
+                    Close();
+                    break;
+            }
         }
     }
 }
